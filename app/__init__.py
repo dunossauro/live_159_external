54b98f362cdb2ed50d977688f7c6e80daaeaf3c7
dunossauro <mendesxeduardo@gmail.com>
2021-03-15 09:36:23 -0300

    Downgrade python version to heroku

--- app/__init__.py
+++ app/__init__.py
@@ -7,10 +7,26 @@ from validate_docbr import CPF
 
 cpf = CPF()
 
+swagger_config = {
+    'headers': [
+    ],
+    'specs': [
+        {
+            'endpoint': 'apispec_1',
+            'route': '/apispec_1.json',
+            'rule_filter': lambda rule: True,
+            'model_filter': lambda tag: True,
+        }
+    ],
+    'static_url_path': '/flasgger_static',
+    'swagger_ui': True,
+    'specs_route': '/'
+}
+
 
 def create_app():
     app = Flask(__name__)
-    swagger = Swagger(app)
+    swagger = Swagger(app, config=swagger_config)
 
     @app.route('/check-cpf')
     def check_cpf():
